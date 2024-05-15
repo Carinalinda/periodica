283cb4335b5da3eff6c0fc7cda88208bbcbaf1f3
Carina Camargo <catcatarina193@gmail.com>
2024-05-15 17:46:04 -0300

    Tabela Periodica

---
+++ tabela periodica.py
@@ -0,0 +1,12 @@
+elemento = input("digite um elemento: ")
+elemento = elemento.lower()
+if elemento == "h":
+    print("Hidrogenio, numero atomico 1")
+elif elemento == "na":
+    print("Sódio, numero atomico 11")
+elif elemento == "ca":
+    print("Cálcio, numero atomico 20")
+elif elemento == "li":
+    print("Lítio, numero atomico 3")
+else: 
+    print("nao sei dizer qual é esse elemento") 
